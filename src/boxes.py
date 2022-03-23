67a8a8e445d35114119d0e1d854ee291a7a730c7
doctorMcbob <wesley@abe.ai>
2022-03-22 23:07:53 -0700

    forgot the boxes

---
+++ src/boxes.py
@@ -0,0 +1,16 @@
+HITBOXES = {}
+HURTBOXES = {}
+
+def load():
+    from src.lib import BOXES as B
+
+    for key in B.HITBOXES:
+        HITBOXES[key] = B.HITBOXES[key]
+        HURTBOXES[key] = B.HURTBOXES[key]
+        
+def get_hitbox_map(name):
+    return HITBOXES[name] if name in HITBOXES else None
+
+def get_hurtbox_map(name):
+    return HURTBOXES[name] if name in HURTBOXES else None
+
