c36c149ac31c2414cfd8605aa2f1ba6366edd75f
doctorMcbob <wesley@abe.ai>
2023-01-07 14:20:46 -0800

    Gold coins

--- boot.py
+++ boot.py
@@ -1,17 +1,18 @@
-from src import game
-from src import editor
-from src import tas
 import sys
 
 if __name__ == "__main__":
     if "-e" in sys.argv or "--editor" in sys.argv:
+        from src import editor
         G = editor.set_up()
         editor.run(G)
     elif "-t" in sys.argv:
+        from src import tas
         G = tas.set_up()
         tas.run(G)
     else:
+        from src import game
         if "-l" in sys.argv or "--load" in sys.argv:
+            from src import editor
             editor.load()
             editor.load_all_templates_button(0)
             editor.save(True)
