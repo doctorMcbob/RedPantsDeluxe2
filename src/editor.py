daf1cac408976b14c5bd8175cba59ea64cf08582
Wesley Wooten <wootenwesley@gmail.com>
2024-07-19 14:12:37 -0700

    added menu bar for editor v3 :3)-<

--- src/editor.py
+++ src/editor.py
@@ -10,6 +10,8 @@ from pygame.locals import *
 import sys
 import os
 
+from pprint import pformat
+
 from src import game
 from src import inputs
 from src import frames
@@ -18,7 +20,102 @@ from src import sprites
 from src import boxes
 from src import actor
 from src import worlds
+from src.editor_menuing import MenuHeader
 
+# # # # # # # #
+# Soft Layer  #
+#-------------#
+# to be saved #
+# and loaded  #
+#    over     #
+# good idea.? #
+# # # # # # # #
+WORLDS = {}
+SPRITESHEETS = {}
+ACTORS = {}
+SCRIPTS = {}
+SPRITEMAPS = {}
+OFFSETS = {}
+TILEMAPS = {}
+HITBOXES = {}
+HURTBOXES = {}
+
+# ~~~ globals ~~~
+SAVED = True
+IMG_LOCATION = "img/"
+SCRIPT_LOCATION = "scripts/"
+WORLD_TEMPLATE = {"actors":[], "background":None, "x_lock": None, "y_lock": None}
+
+SCROLLER = {
+    "X": 0, "Y": 0,
+    "CX": 0, "CY": 0
+}
+# ~~~ menu functions ~~~
+def load():
+    global WORLDS, SPRITESHEETS, SPRITEMAPS, ACTORS, SCRIPTS, OFFSETS, HITBOXES, HURTBOXES, TILEMAPS
+    from src.lib import WORLDS as W
+    from src.lib import SPRITESHEETS as S
+    from src.lib import ACTORS as A
+    from src.lib import SCRIPTS as SC
+    from src.lib import BOXES as B
+    WORLDS = W.WORLDS
+    SPRITESHEETS = S.SPRITESHEETS
+    SPRITEMAPS = S.SPRITEMAPS
+    OFFSETS = S.OFFSETS
+    TILEMAPS = S.TILEMAPS
+    ACTORS = A.ACTORS
+    SCRIPTS = SC.SCRIPTS
+    HITBOXES = B.HITBOXES
+    HURTBOXES = B.HURTBOXES
+
+def save(noload=False):
+    global SAVED
+    with open("src/lib/WORLDS.py", "w+") as f:
+        f.write("WORLDS = {}".format(pformat(WORLDS)))
+    with open("src/lib/SPRITESHEETS.py", "w+") as f:
+        f.write("SPRITESHEETS = {}\nSPRITEMAPS = {}\nOFFSETS = {}\nTILEMAPS = {}".format(
+            pformat(SPRITESHEETS), pformat(SPRITEMAPS), pformat(OFFSETS), pformat(TILEMAPS))
+        )
+    with open("src/lib/ACTORS.py", "w+") as f:
+        f.write("ACTORS = {}".format(pformat(ACTORS)))
+    with open("src/lib/SCRIPTS.py", "w+") as f:
+        f.write("SCRIPTS = {}".format(pformat(SCRIPTS)))
+    with open("src/lib/BOXES.py", "w+") as f:
+        f.write("HITBOXES = {}\nHURTBOXES = {}".format(pformat(HITBOXES), pformat(HURTBOXES)))
+    
+    SAVED = True
+    if noload: return
+    load_game()
+
+def off(*args, **kwargs): pass
+# ~~~ menu buttons ~~~
+MENU_ITEMS = {
+    "File": {
+        "Load": load,
+        "Save": save,
+        "----------": off,
+        "quit": quit
+    },
+    
+}
+
+def load_game():
+    sprites.swap_in(offsets=OFFSETS, sprite_maps=SPRITEMAPS, tile_maps=TILEMAPS)
+    scripts.swap_in(SCRIPTS)
+    worlds.swap_in(WORLDS)
+    actor.swap_in(ACTORS)
+    boxes.swap_in(hitboxes=HITBOXES, hurtboxes=HURTBOXES)
+
+def draw(G):
+    CX, CY = SCROLLER["CX"], SCROLLER["CY"]
+    frame = frames.get_frame("EDITOR_VIEW")
+    frame.scroll_x = CX
+    frame.scroll_y = CY
+    world = G["WORLDS"].get_world(G["WORLD"])
+    frame.world = world
+    drawn = frame.drawn(DEBUG=G)
+    G["SCREEN"].blit(drawn, (0, 32))
+    
 def set_up():
     pygame.init()
     pygame.mixer.init()
@@ -32,8 +129,10 @@ def set_up():
     inputs.add_state("PLAYER2")
 
     load()
-#    load_game()
+    load_game()
 
+    header = MenuHeader(G["SCREEN"], MENU_ITEMS)
+    G["HEADER"] = header
     from src import printer
     printer.GIF_SIZE = 30 * 4
     G["PRINTER"] = printer
@@ -48,7 +147,13 @@ def set_up():
 
 def update_frames(G):
     frames.clear()
-    frames.add_frame("EDITOR_VIEW", G["WORLD"], (1152, 640))
+    frames.add_frame(
+        "EDITOR_VIEW",
+        G["WORLD"], (
+            G["SCREEN"].get_width(),
+            G["SCREEN"].get_height() - 32
+        )
+    )
     frames.add_frame("MAIN", G["WORLD"], (1152, 640))
     G["FRAMEMAP"] = {
         "MAIN": (0, 0),
@@ -56,4 +161,11 @@ def update_frames(G):
 
 def run(G):
     while True:
+        draw(G)
         update_frames(G)
+        G["HEADER"].update()
+        pygame.display.update()
+        G["HEADER"].CLICK = False
+        for e in pygame.event.get():
+            if e.type == pygame.MOUSEBUTTONUP:
+                G["HEADER"].CLICK = True
---
+++ src/editor_menuing.py
@@ -0,0 +1,128 @@
+import pygame
+from pygame import Surface, Rect
+
+class MenuHeader:
+    MENU_BG = (100, 100, 100)
+    MENU_BG_ALT = (200, 200, 200)
+    MENU_BG_SEL  = (150, 150, 150)
+    MENU_TXT = (255, 255, 255)
+    MENU_TXT_SEL = (255, 200, 200)
+
+    # when a click happens this will be True otherwise it will be False
+    CLICK = False
+
+    def __init__(self, destination, template, pos=(0, 0)):
+        self.HEL16 = pygame.font.SysFont("Helvetica", 16)
+
+        self.menu_items = template
+        self.pos = pos
+        self.destination = destination
+        # Active submenus
+        # key "File", value { ... } or Callable
+        self.sub_menus = {}
+        self.path = set()
+
+    def draw_header(self):
+        w = self.destination.get_width()
+        surf = Surface((w, 32))
+        surf.fill(self.MENU_BG)
+        w_ = 4
+        mpos = pygame.mouse.get_pos()
+
+        picked = None, None
+
+        for item in self.menu_items:
+            text = self.HEL16.render(item, 0, self.MENU_TXT)
+            rect = Rect((self.pos[0] + w_, self.pos[1]), (text.get_width(), 32))
+            if rect.collidepoint(mpos):
+                text = self.HEL16.render(item, 0, self.MENU_TXT_SEL)
+                pygame.draw.rect(surf, self.MENU_BG_SEL, Rect((w_, 0), (text.get_width(), 32)))
+                if self.CLICK:
+                    picked = item, (w_, 32)
+
+            surf.blit(text, (w_, 8))
+
+            pygame.draw.line(surf, self.MENU_BG_ALT, (w_, 26), (w_ + text.get_width(), 26))
+            w_ += text.get_width() + 4
+
+            pygame.draw.line(surf, self.MENU_BG_ALT, (w_, 8), (w_, 26))
+
+            w_ += 4
+
+        self.destination.blit(surf, self.pos)
+
+        return picked
+
+    def draw_dropdown(self, items, pos=(0, 0)):
+        if not items:
+            return None, None
+        h = len(items) * 32
+        longest = max(
+            list(items.keys()),
+            key=lambda s: self.HEL16.render(s, 0, (0, 0, 0)).get_width()
+        )
+        _longest = self.HEL16.render(longest, 0, self.MENU_TXT)
+        w = _longest.get_width() + 16
+
+        surf = Surface((w, h))
+        surf.fill(self.MENU_BG)
+
+        mpos = pygame.mouse.get_pos()
+        #print(mpos)
+        h_ = 0
+
+        picked = None, None
+
+        for item in items:
+            text = self.HEL16.render(item, 0, self.MENU_TXT)
+            rect = Rect((pos[0], pos[1] + h_), (w, 32))
+            if rect.collidepoint(mpos):
+                text = self.HEL16.render(item, 0, self.MENU_TXT_SEL)
+                pygame.draw.rect(surf, self.MENU_BG_SEL, Rect((0, h_), (w, 32)))
+                if self.CLICK:
+                    print("HERE!?")
+                    picked = item, (pos[0] + w, pos[1] + h_)
+            surf.blit(text, (8, h_ + 8))
+            h_ += 32
+            pygame.draw.line(surf, self.MENU_BG_ALT, (8, h_), (w-8, h_))
+
+        self.destination.blit(surf, pos)
+
+        return picked
+
+    def update(self):
+        mkey = self.draw_header()
+        submenu, pos = mkey
+        if submenu is not None:
+            if isinstance(self.menu_items[submenu], dict):
+                self.sub_menus = {}
+                if self.menu_items[submenu]:
+                    self.sub_menus[(submenu, pos)] = self.menu_items[submenu]
+                    self.path.add(submenu)
+            else:
+                self.menu_items[submenu]()
+
+        for key in list(self.sub_menus.keys()):
+            if key not in self.sub_menus:
+                continue
+            _, pos = key
+            choice, pos2 = self.draw_dropdown(self.sub_menus[key], pos)
+            if choice is None:
+                continue
+
+            if isinstance(self.sub_menus[key][choice], dict):
+                _sub_menus = {}
+                for name, pos in self.sub_menus:
+                    if name in self.path:
+                        _sub_menus[(name, pos)] = self.sub_menus[(name, pos)]
+                self.sub_menus = _sub_menus
+                if self.sub_menus[key][choice]:
+                    self.sub_menus[(choice, pos2)] = self.sub_menus[key][choice]
+                    self.path.add(choice)
+                else:
+                    self.sub_menus = {}
+                    self.path.clear()
+            else:
+                self.sub_menus[key][choice]()
+                self.sub_menus = {}
+                self.path.clear()
